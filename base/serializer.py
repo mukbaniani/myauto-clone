7023650df41d2bc36cef5c858f54de227108ece5
mukbaniani <mukbaniani.11@gmail.com>
2022-02-27 16:57:31 +0400

    test

--- base/serializer.py
+++ base/serializer.py
@@ -47,6 +47,7 @@ class EmployeeSerializer(serializers.ModelSerializer):
         else:
             raise serializers.ValidationError(_("პაროლის შეყვანა აუცილებელია"))
         return attrs
+        
 
     def create(self, validated_data):
         password = validated_data.get("password1")
